016441af8e399f50f5409d07092d6799680b3a58
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-03-16 10:12:13 -0700

    fix index page text

--- lib/the_brogrammer_web/controllers/page_controller.ex
+++ lib/the_brogrammer_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule TheBrogrammerWeb.PageController do
   use TheBrogrammerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    assigns = [
+      page_title: "Welcome"
+    ]
+
+    render(conn, "index.html", assigns)
+  end
+
+  def about_me(conn, _params) do
+    assigns = [
+      page_title: "About me"
+    ]
+
+    render(conn, "about_me.html", assigns)
   end
 end
