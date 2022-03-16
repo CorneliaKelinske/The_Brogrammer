82cf3c1d0c93bd7f325351673c0634b542a2042c
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-03-16 16:43:56 -0700

    fix error in runtime.ex

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,7 +56,7 @@ if config_env() == :prod do
 
   config :swoosh, local: false
 
-  config :the_little_thinkers_space, TheLittleThinkersSpace.Mailer,
+  config :the_brogrammer, TheBrogrammer.Mailer,
     adapter: Swoosh.Adapters.Mailgun,
     api_key: System.get_env("MAILGUN_API_KEY"),
     domain: System.get_env("MAILGUN_DOMAIN")
