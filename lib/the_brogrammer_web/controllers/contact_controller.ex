dd1a4c34a534b84c0270e32dda728c46e2ab9dfa
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-03-23 14:10:36 -0700

    change an function name

--- lib/the_brogrammer_web/controllers/contact_controller.ex
+++ lib/the_brogrammer_web/controllers/contact_controller.ex
@@ -5,7 +5,7 @@ defmodule TheBrogrammerWeb.ContactController do
   @spec new(Plug.Conn.t(), map) :: Plug.Conn.t()
   def new(conn, _params) do
     with {captcha_text, captcha_image} <- ExRoboCop.create_captcha() do
-      form_id = ExRoboCop.create_form_ID(captcha_text)
+      form_id = ExRoboCop.create_form_id(captcha_text)
 
       render(conn, "new.html",
         page_title: "Contact",
@@ -56,7 +56,7 @@ defmodule TheBrogrammerWeb.ContactController do
 
   defp render_page(conn, changeset, message_type, message) do
     with {captcha_text, captcha_image} <- ExRoboCop.create_captcha() do
-      id = ExRoboCop.create_form_ID(captcha_text)
+      id = ExRoboCop.create_form_id(captcha_text)
 
       conn
       |> put_flash(message_type, message)
